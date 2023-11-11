b6cef7f92cd2f8b50a5da539d988f9d1886c233d
Javier Cebrián <jac.cebrian@gmail.com>
2023-11-11 20:26:18 +0100

    Clean and update

--- optimizer_display_panel.py
+++ optimizer_display_panel.py
@@ -51,13 +51,16 @@ class GraphApp:
         self.evaluation_arr = None
         self.function_expression = None
 
+    def read_limits(self):
+        self.lower_limit = self.lower_limit_input_widget.value
+        self.upper_limit = self.upper_limit_input_widget.value
+
+        if self.lower_limit == self.upper_limit:
+            self.text_widget.value = "Limits must have different values."
 
     def start_optimization(self, event):
         seed = 42
         np.random.seed(seed)
-
-        self.lower_limit = self.lower_limit_input_widget.value
-        self.upper_limit = self.upper_limit_input_widget.value
 
         self.x0 = np.random.uniform(self.lower_limit,
                                     self.upper_limit)
@@ -71,9 +74,7 @@ class GraphApp:
         else:
             self.function_expression = self.functions_dict[self.selected_option]
 
-
-
-        x = self.x # eval expression needs x value
+        x = self.x  # eval expression needs x value
         self.y = eval(self.function_expression)
 
         self.curve = self.plot_function()
@@ -136,7 +137,6 @@ class GraphApp:
         except Exception as error:
             self.text_widget.value = error
 
-
     def select_function(self, event):
         self.selected_option = event.obj.value
         self.text_widget.value = "Selected function is " + self.selected_option +\
@@ -147,12 +147,11 @@ class GraphApp:
             self.check_function_button.visible = False
         else:
             self.custom_input_widget.visible = False
+            self.check_function_button.visible = False
 
         self.lower_limit_input_widget.visible = True
         self.upper_limit_input_widget.visible = True
         self.optimize_button.visible = True
-
-
 
     def advance_plot(self, event):
         if self.index < self.max_index:
