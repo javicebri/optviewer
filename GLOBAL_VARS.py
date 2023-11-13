9adee15c8a7b7d19991de7d4f90eb521c6d73524
Javier Cebrián <jac.cebrian@gmail.com>
2023-11-13 17:53:05 +0100

    Add global vars dicts

--- GLOBAL_VARS.py
+++ GLOBAL_VARS.py
@@ -1,5 +1,32 @@
 
 
 # Preset functions
-functions_dict = {'Cuadratic': 'x**2',
-                               'Mixed': '(np.sin(1/2 * x) + 2 * np.cos(1/2 * x)) * x**2'}
+functions_dict = {"Quadratic": "x**2",
+                  "Mixed": "(np.sin(1/2 * x) + 2 * np.cos(1/2 * x)) * x**2"
+                  }
+
+# Basic args
+basic_args = {
+    "jac": None,
+    "hess": None,
+    "hessp": None,
+    "bounds": None,
+    "constraints": (),
+    "tol": None,
+    "callback": None,
+    "options": None
+}
+
+# Preset Methods
+methods_options_dict = {
+    "SLSQP": {"ftol": ["float", None],
+                "eps": ["float", None],
+                #"disp": ["bool", False],
+                "maxiter": ["int", None],
+                "finite_diff_rel_step": ["array", None]},
+    "COBYLA": {"rhobeg": ["float", None],
+                "tol": ["float", None],
+                #"disp": ["bool", False],
+                "maxiter": ["int", None],
+                "catol": ["float", None]}
+}
