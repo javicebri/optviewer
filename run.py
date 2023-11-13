e8ce88878d109a8252e8b2c870922fafe9a889d7
Javier Cebrián <jac.cebrian@gmail.com>
2023-11-13 18:10:53 +0100

    Refactor main.py name

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import subprocess
 import webbrowser
 
-# Directorio donde se encuentra el archivo optimizer_display_panel.py
+# Directorio donde se encuentra el archivo main.py
 working_directory = "C:\\Users\\jc_ce\\Desktop\\01Proyectos\\optviewer\\"
 
 # Comando para iniciar la aplicación de Bokeh
-command = f"bokeh serve {working_directory}optimizer_display_panel.py --show"
+command = f"bokeh serve {working_directory}main.py --show"
 
 # Iniciar la aplicación de Bokeh en segundo plano
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
